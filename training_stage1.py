c5aed0708bfb98f66b25a6d73b9a3b2f20c7c7de
tmanh <tmanh93@outlook.com>
2024-09-29 11:58:37 +0200

    updated

--- training_stage1.py
+++ training_stage1.py
@@ -179,7 +179,7 @@ def main(args) -> None:
             dst_cs = dst_cs[..., py:py+ps, px:px+ps]
             src_cs = src_cs[..., py:py+ps, px:px+ps]
 
-            loss = l1(raw, src_cs) + cobi(raw.view(N * V, -1, ps, ps), src_cs[:, 0])
+            loss = l1(raw, src_cs) + cobi(raw.view(N * V, -1, ps, ps), src_cs)
 
             opt.zero_grad()
             accelerator.backward(loss)
