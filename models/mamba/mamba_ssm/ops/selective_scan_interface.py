1a3139dc99b99929e4f676d19868e772afad471e
tmanh <tmanh93@outlook.com>
2024-10-22 20:28:43 +0200

    update

--- models/mamba/mamba_ssm/ops/selective_scan_interface.py
+++ models/mamba/mamba_ssm/ops/selective_scan_interface.py
@@ -2,7 +2,7 @@
 
 import torch
 import torch.nn.functional as F
-from torch.cuda.amp import custom_bwd, custom_fwd
+from torch.amp import custom_bwd, custom_fwd
 
 from einops import rearrange, repeat
 
