1d694198e2d1da89c73e4fa4bfb2a702d1eaa392
tmanh <tmanh93@outlook.com>
2024-10-22 21:12:24 +0200

    update

--- models/mamba/mamba_ssm/ops/selective_scan_interface.py
+++ models/mamba/mamba_ssm/ops/selective_scan_interface.py
@@ -156,10 +156,10 @@ class MambaInnerFnNoOutProj(torch.autograd.Function):
 
     @staticmethod
     @custom_fwd
-    def forward(ctx, xz, conv1d_weight, conv1d_bias, x_proj_weight, delta_proj_weight,
-                *, device_type: str,
-                A=None, B=None, C=None, D=None, delta_bias=None, B_proj_bias=None,
-                C_proj_bias=None, delta_softplus=True, checkpoint_lvl=1):
+    def forward(
+        ctx, xz, conv1d_weight, conv1d_bias, x_proj_weight, delta_proj_weight, *, device_type: str,
+        A=None, B=None, C=None, D=None, delta_bias=None, B_proj_bias=None,
+        C_proj_bias=None, delta_softplus=True, checkpoint_lvl=1):
         """
              xz: (batch, dim, seqlen)
         """
