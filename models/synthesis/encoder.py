2755f053f42e0cafbb0a180caed0c1ee1935becb
tmanh <tmanh93@outlook.com>
2024-10-22 11:43:35 +0200

    update

--- models/synthesis/encoder.py
+++ models/synthesis/encoder.py
@@ -10,64 +10,17 @@ class SwinColorFeats(nn.Module):
     def __init__(self):
         super().__init__()
         
-        # self.backbone = SwinTransformerV2(window_size=8)
-        # self.backbone.load_pretrained()
-        # self.backbone.eval()
-
-        self.backbone = models.vgg19(pretrained=True).features
+        self.backbone = SwinTransformerV2(window_size=8)
+        self.backbone.load_pretrained()
         self.backbone.eval()
 
-        self.selected_layers = [3, 8, 17, 26, 35]  # Correspond to different conv/pool layers in VGG19
-
-        self.mean = torch.tensor([0.485, 0.456, 0.406]).view(1, 3, 1, 1)
-        self.std = torch.tensor([0.229, 0.224, 0.225]).view(1, 3, 1, 1)
-
-        self.pre_conv = nn.ModuleList(
-            [
-                nn.Sequential(
-                    nn.ReLU(inplace=True),
-                    nn.Conv2d(512, 32, 3, 1, 1),
-                ),
-                nn.Sequential(
-                    nn.ReLU(inplace=True),
-                    nn.Conv2d(512, 32, 3, 1, 1),
-                ),
-                nn.Sequential(
-                    nn.ReLU(inplace=True),
-                    nn.Conv2d(256, 32, 3, 1, 1),
-                ),
-                nn.Sequential(
-                    nn.ReLU(inplace=True),
-                    nn.Conv2d(128, 32, 3, 1, 1),
-                ),
-                None
-            ]
-        )
-
     def forward(self, colors):
-        self.mean = self.mean.to(colors.device)
-        self.std = self.std.to(colors.device)
-
-        colors = (colors - self.mean) / self.std
-
         B, V, C, H, W = colors.shape
         with torch.no_grad():
-            feats = []
-    
-            x = colors.view(-1, C, H, W)
-            for i, layer in enumerate(self.backbone):
-                x = layer(x)
-                if i in self.selected_layers:
-                    feats.append(x)
-
-        hf, wf = colors.shape[-2:]
-        merge = []
-        for i, f in enumerate(feats[::-1]):
-            if self.pre_conv[i] is not None:
-                f = self.pre_conv[i](f)
-                f = F.interpolate(f, size=(hf, wf), mode='nearest')
-            merge.append(f)
-
+            feats = self.backbone(colors.view(B * V, C, H, W))
+        for f in feats:
+            print(f.shape)
+        exit()
         return torch.cat(merge, dim=1).view(B, V, -1, hf, wf)
     
     def freeze(self):
