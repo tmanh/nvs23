1000b811290430287fee3e9ae22d2797e6a16c02
B800 <b800@telin>
2025-02-13 15:17:27 +0100

    check

--- training_stage1.py
+++ training_stage1.py
@@ -77,7 +77,7 @@ def main(args) -> None:
 
     # Prepare models for training:
     renderer.to_train().to(device)
-    renderer, opt, loader, val_loader = accelerator.prepare(renderer, opt, loader, val_loader)
+    renderer, opt, loader, val_loader = accelerator.prepare(renderer, opt, loader, val_loader, find_unused_parameters=True)
     pure_renderer = accelerator.unwrap_model(renderer)
 
     # Variables for monitoring/logging purposes:
